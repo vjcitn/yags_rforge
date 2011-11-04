a7a5056ea27ad1821b3ffb4e77d5b0671c810551
stvjc <stvjc@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-04 14:16:17 +0000

    prepare r-forge image with no deprecate msg
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/yags@54 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/edd/R/zzz.R
+++
@@ -1,7 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-    msg <- strwrap(sprintf(
-              "*** Deprecation warning ***: Package '%s' is deprecated
-                   and will not be supported after Bioconductor
-                   release %s.", pkgname, "2.9"), exdent=4)
-    packageStartupMessage(paste(msg, collapse="\n"))
-}
